8f0bd5983bc5873cb3540864aebf279e930d0eb5
Paul Oostenrijk <paul@glemma.nl>
2019-01-29 11:13:48 +0100

    feat(csv): Find duplicates in student names and make sure to add count
    feat(html): Updated some of the widths to make sure it will be a row

--- csv.js
+++ csv.js
@@ -46,9 +46,28 @@ async function parseRecords(records, start, end, viewOptions) {
 
         const images = studentCompetencies.map(item => `./images/${item[1]}`)
 
-        const pages = Math.floor(images / 15)
+        // Validate of all images are existent
+        images.forEach((image) => {
+            if (fs.existsSync(image) === false) {
+                console.error(`Cannot retrieve "${image}", does not exist`)
+            }
+        })
 
-        generatePDF(`./output/${student}.pdf`, {
+        const duplicates = students.slice(0, index).reduce((number, otherStudent) => {
+            if (student === otherStudent) {
+                return number + 1
+            }
+
+            return number
+        }, 0)
+
+        let filename = `${student}`
+
+        if (duplicates > 0) {
+            filename = `${filename}-(${duplicates})`
+        }
+
+        generatePDF(`./output/${filename}.pdf`, {
             studentName: student,
             year: 'SP',
             category: category,
@@ -65,7 +84,7 @@ program
 .version('0.1.0')
 .arguments('<path> <start> <end>')
 .option('-y --year', 'What year', 'PS')
-.option('-B --background', 'What background', '#ff3399')
+.option('-B --background <background>', 'What background', '#ff3399')
 .action(function (path, start, end, options) {
     return loadCSV(path).then((records) => {
         const startDate = moment(start, dateFormat)
